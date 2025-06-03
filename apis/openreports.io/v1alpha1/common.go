494c1ada191bc2eb929ea73e0eea89ea584ccd4f
Your Name <you@example.com>
2025-06-03 16:51:39 +0300

    report interface
    Signed-off-by: Your Name <you@example.com>

--- apis/openreports.io/v1alpha1/common.go
+++ apis/openreports.io/v1alpha1/common.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Status specifies state of a policy result
@@ -77,4 +78,17 @@ func ToResourceString(res *corev1.ObjectReference) string {
 	}
 
 	return resource
+}
+
+type ReportInterface interface {
+	metav1.Object
+	GetID() string
+	GetKey() string
+	GetScope() *corev1.ObjectReference
+	GetResults() []ReportResult
+	HasResult(id string) bool
+	GetSummary() []ReportSummary
+	GetSource() string
+	GetKinds() []string
+	GetSeverities() []string
 }
